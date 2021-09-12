6e7cf5d1fb8b38287c2c5c85c065197d7f2f6675
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2021-09-12 18:54:15 +1000

    UPDATE Task.java - stub methods from UML in prep for TDD

--- src/main/java/com/daylog/Task.java
+++ src/main/java/com/daylog/Task.java
@@ -1,5 +1,7 @@
 package com.daylog;
 
+import com.daylog.exceptions.NotYetImplementedException;
+
 import java.time.LocalDateTime;
 
 public class Task {
@@ -8,4 +10,27 @@ public class Task {
     String description;
     LocalDateTime dateTime;
     TaskType type;
+
+    public Task(String name, String description, TaskType type) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        throw new NotYetImplementedException();
+    }
+
+    public String getName() {
+        throw new NotYetImplementedException();
+    }
+
+    public String getDescription() {
+        throw new NotYetImplementedException();
+    }
+
+    public String getType() {
+        throw new NotYetImplementedException();
+    }
 }
