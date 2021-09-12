bc5c26eef20a471409679a2f9e34ae53e6e61e82
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2021-09-12 18:50:42 +1000

    CREATE NotYetImplementedE.java - basic stub exception for TDD

---
+++ src/main/java/com/daylog/exceptions/NotYetImplementedException.java
@@ -0,0 +1,23 @@
+package com.daylog.exceptions;
+
+public class NotYetImplementedException extends RuntimeException
+{
+    /**
+     * @deprecated Deprecated to remind you to implement the corresponding code
+     *             before releasing the software.
+     */
+    @Deprecated
+    public NotYetImplementedException()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated to remind you to implement the corresponding code
+     *             before releasing the software.
+     */
+    @Deprecated
+    public NotYetImplementedException(String message)
+    {
+        super(message);
+    }
+}
