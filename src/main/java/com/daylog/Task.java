5b0cabd14e8ef4494eb82efc5395023683e60a72
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2021-09-12 16:32:51 +1000

    UPDATE Task.java - add fields to reflect UML

--- src/main/java/com/daylog/Task.java
+++ src/main/java/com/daylog/Task.java
@@ -1,5 +1,11 @@
 package com.daylog;
 
+import java.time.LocalDateTime;
+
 public class Task {
+    Integer id;
+    String name;
     String description;
+    LocalDateTime dateTime;
+    TaskType type;
 }
